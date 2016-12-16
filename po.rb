1957f1655208c230dc03dc8e5de10888d899d273
Nathan Robinson <nrobinson2000@me.com>
2016-12-15 23:45:23 -0500

    at 23:45 of 15-12-2016

--- po.rb
+++ po.rb
@@ -5,7 +5,7 @@ class Po < Formula
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/po-util', :using => :git
 
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/2.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/3.0.tar.gz'
   sha256 '956bf7c4f12b4895c89ef5dccd40a5406583507d1a2bc01f5c2cfa85f76f04d5'
 
   def install
