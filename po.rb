566417041da6347a54be1cce0716a38efa0074a3
Nathan Robinson <nrobinson2000@me.com>
2016-12-16 00:02:26 -0500

    at 00:02 of 16-12-2016

--- po.rb
+++ po.rb
@@ -5,11 +5,18 @@ class Po < Formula
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
 
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/3.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/4.0.tar.gz'
   sha256 '06fe540799b1141536b4129e863dc618fc84d6d1bd03549d869f8de637f88e6d'
 
   def install
     bin.install 'po'
+
+  end
+
+  def caveats; <<-EOS.undent
+  In order to complete the installation of po-util,
+  you must run 'po install'.
+  EOS
   end
 
 end
