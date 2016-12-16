0141abf30f5be663da40c90df7cc8318a1d91309
Nathan Robinson <nrobinson2000@me.com>
2016-12-16 00:22:57 -0500

    [skip ci] at 00:22 of 16-12-2016

---
+++ config-expect.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/expect -f
+set timeout 10
+
+spawn /usr/local/bin/po config
+
+expect -exact "Branch: "
+send -- "release/stable\n"
+expect -exact "Baud Rate: "
+send -- "po\n"
+
+interact
