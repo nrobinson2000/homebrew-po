dc2428c08a0e2588f3f91ca13012a582a365857a
Nathan Robinson <nrobinson2000@me.com>
2016-12-28 13:35:37 -0500

    [skip ci] Add support for core at 13:35 of 28-12-2016

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/28.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/29.0.tar.gz'
   sha256 '6ed7fffd1bc8f045625f7cb1927c061896fbddfd314ae62dc4782f82e1b54fc8'
 
   def install
