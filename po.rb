98f8196830faa6fd924e2bed3cab15b4c849ffbe
Nathan Robinson <nrobinson2000@me.com>
2016-12-29 13:09:18 -0500

    [skip ci] Fixed core, Library commands automatically add and remove includes at 13:09 of 29-12-2016

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/31.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/32.0.tar.gz'
   sha256 '48f7e37b199b2991d55ccda474d90dad05bb7f3252cd14be093ae38e47f5947d'
 
   def install
