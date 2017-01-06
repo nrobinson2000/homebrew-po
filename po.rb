7cf2fd34b8f52fc49fef4cd93ef3d9a8e04bdfcf
Nathan Robinson <nrobinson2000@me.com>
2017-01-06 08:29:01 -0500

    [skip ci] Created functions for common library manager routines at 08:29 of 06-01-2017

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/35.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/36.0.tar.gz'
   sha256 'fcdf6daaa615393111665177dfa7c747b555f3c9b1880f4f6e57bc43a3895148'
 
   def install
