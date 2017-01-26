9215d7e6aa10ccdca0375256604ac6d11b52d12e
Nathan Robinson <nrobinson2000@me.com>
2017-01-26 14:33:26 -0500

    [skip ci] Particle libs 2.0 support at 14:33 of 26-01-2017

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/41.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/42.0.tar.gz'
   sha256 'fde4f0e080a64a360b9b3df6d661998ef76594ae0a3b1f68366a50f02435a282'
 
   def install
