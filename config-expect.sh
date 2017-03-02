9e4daaafc583836d2b7ef9c1c254788250f52e9a
Nathan Robinson <nrobinson2000@me.com>
2017-03-02 18:48:05 -0500

    [skip ci] Add support for redbear duo at 18:48 of 02-03-2017

--- config-expect.sh
+++ config-expect.sh
@@ -5,6 +5,8 @@ spawn /usr/local/bin/po config
 
 expect -exact "Branch: "
 send -- "release/stable\n"
+expect -exact "Branch: "
+send -- "duo\n"
 expect -exact "Baud Rate: "
 send -- "po\n"
 expect -exact "(yes/no): "
--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/55.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/56.0.tar.gz'
   sha256 'c04d5e68648c3ea333397b8ce1e85de6bf3b370036ae5ddcf1f34e172d6cf31d'
 
   def install
