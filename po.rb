a09d6c0a68bc6a5f33c4d80b590dd142c2cda4a9
Nathan Robinson <nrobinson2000@me.com>
2017-03-02 18:58:51 -0500

    [skip ci] Add support for redbear duo at 18:58 of 02-03-2017

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/57.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/58.0.tar.gz'
   sha256 '88fa6fd0a9b9960a5c8ca3e53a5f3883aafe9257171d89fe17f0879692672860'
 
   def install
