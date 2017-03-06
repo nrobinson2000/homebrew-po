b449cd066ed3b2873b3d77162282a95694984f74
Nathan Robinson <nrobinson2000@me.com>
2017-03-06 10:51:26 -0500

    [skip ci] Disable _echo colors if not in a terminal at 10:51 of 2017-03-06

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/59.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/60.0.tar.gz'
   sha256 '29b6ea7ee5e0f99dade7d1b3df10714c235bb7f85630b738cd57fb87c38e674c'
 
   def install
