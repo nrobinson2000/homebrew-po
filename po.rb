305ed02be3b72ac10ad4c8e6c7baaf29367b7d08
Nathan Robinson <nrobinson2000@me.com>
2017-04-01 12:15:09 -0400

    init now adds ci and git at 12:15 of 01-04-2017

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience for Linux and macOS"
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/65.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/66.0.tar.gz'
   sha256 '0e4614d7528ba037202727fb582c92ca8223aebc3dd06636e8a1c22cc908c0a0'
 
   def install
