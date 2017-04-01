40e6bf30a5297110254205106d857a5934bd70a6
Nathan Robinson <nrobinson2000@me.com>
2017-04-01 12:16:44 -0400

    init now adds ci and git at 12:16 of 01-04-2017

--- po.rb
+++ po.rb
@@ -3,7 +3,7 @@ class Po < Formula
   homepage 'https://github.com/nrobinson2000/po-util'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
   url 'https://github.com/nrobinson2000/homebrew-po/archive/66.0.tar.gz'
-  sha256 '0e4614d7528ba037202727fb582c92ca8223aebc3dd06636e8a1c22cc908c0a0'
+  sha256 '2edd6ce8a5151211127dfa595ccd5455d9a615f176cda9ea082f99d934cb8a06'
 
   def install
     bin.install 'po'
