8fe376ec19183666d9a8e912ed789e680457af16
Nathan Robinson <nrobinson2000@me.com>
2017-06-23 11:08:24 -0400

    [skip ci] at 11:08 of 2017-06-23

--- po.rb
+++ po.rb
@@ -2,7 +2,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/83.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/84.0.tar.gz'
   sha256 '921a54bb75606c6fdf0da72a8cd4422b6e6df3ab4938da9a2ce8075218813d4c'
 
   def install
