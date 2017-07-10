ef490a9c15c68fe18dda4026cb711eb70c3cf010
Nathan Robinson <nrobinson2000@me.com>
2017-07-09 21:24:27 -0400

    [skip ci] at 21:24 of 2017-07-09

--- po.rb
+++ po.rb
@@ -1,8 +1,25 @@
+#                                            __      __  __
+#                                           /  |    /  |/  |
+#     ______    ______           __    __  _██ |_   ██/ ██ |
+#    /      \  /      \  ______ /  |  /  |/ ██   |  /  |██ |
+#   /██████  |/██████  |/      |██ |  ██ |██████/   ██ |██ |
+#   ██ |  ██ |██ |  ██ |██████/ ██ |  ██ |  ██ | __ ██ |██ |
+#   ██ |__██ |██ \__██ |        ██ \__██ |  ██ |/  |██ |██ |
+#   ██    ██/ ██    ██/         ██    ██/   ██  ██/ ██ |██ |
+#   ███████/   ██████/           ██████/     ████/  ██/ ██/
+#   ██ |
+#   ██ |
+#   ██/                  https://po-util.com
+#
+
+#  po-util - The Ultimate Local Particle Experience for Linux and macOS
+# Copyright (C) 2017  Nathan Robinson
+
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/109.0.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/110.0.tar.gz'
   sha256 'c3fc698ed93467abf69f533badc0326549485aa82626f5cf96c89849ece7ce91'
 
   def install
