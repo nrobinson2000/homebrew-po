ec75bfc15bbfbc93face10df8911c0ac08938f01
Nathan Robinson <nrobinson2000@me.com>
2017-10-05 23:11:25 -0400

    [skip ci] parse and display flash and ram usage in a friendly way with percentages at 23:11 of 2017-10-05

--- po.rb
+++ po.rb
@@ -19,7 +19,7 @@ class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
-  url 'https://github.com/nrobinson2000/homebrew-po/archive/127.tar.gz'
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/128.tar.gz'
   sha256 '9ecd53549880601fb3e8c286a4305424aec9c0424a0b2e3a61f9ab434d3091cf'
 
   def install
