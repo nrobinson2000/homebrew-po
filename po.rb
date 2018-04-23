5ea17b41156709d3f80be4807b6c3c9cf0fe5df7
Nathan Robinson <nrobinson2000@me.com>
2018-04-23 10:27:32 -0400

    Update po.rb

--- po.rb
+++ po.rb
@@ -17,7 +17,7 @@ class Po < Formula
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
   url 'https://github.com/nrobinson2000/po/archive/0.4.0.tar.gz'
-  sha256 '8797542977a75d20ac6bc29bc46181a9127df00e89022dc152be47e4234f74d3'
+  sha256 'dca6078c551d62bb38b969e451a315342984d98f5bc2d86cfedaec85831bef3b'
 
   def install
     bin.install 'bin/po'
