64cc475a361490f37603f6f0de8284f18e8b9e78
Nathan Robinson <nrobinson2000@me.com>
2018-04-23 23:38:30 -0400

    Update po.rb

--- po.rb
+++ po.rb
@@ -1,17 +1,3 @@
-#     ______    ______
-#    /      \  /      \
-#   /██████  |/██████  |  The Future of po-util:
-#   ██ |  ██ |██ |  ██ |
-#   ██ |__██ |██ \__██ |      Refactored,
-#   ██    ██/ ██    ██/       Modular,
-#   ███████/   ██████/        Cross-Compatible
-#   ██ |
-#   ██ |
-#   ██/  https://po-util.com
-
-# po - The Ultimate Local Particle Experience for Linux and macOS
-# Copyright (C) 2018 Nathan D. Robinson
-
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage "https://po-util.com"
@@ -36,16 +22,6 @@ class Po < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test po`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-
     system "#{bin}/po", "config", "release/v0.6.3", "duo", "false"
     system "#{bin}/po", "info"
     system "#{bin}/po", "install"
