9a7bb358305df72ba993dd20b4733c1b3ce1a3a8
Nathan Robinson <nrobinson2000@me.com>
2018-04-23 23:35:14 -0400

    Update po.rb

--- po.rb
+++ po.rb
@@ -14,25 +14,25 @@
 
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
-  homepage 'https://po-util.com'
-  url 'https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz'
-  sha256 'f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a'
-  head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
+  homepage "https://po-util.com"
+  url "https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz"
+  sha256 "f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a"
+  head "https://github.com/nrobinson2000/homebrew-po", :using => :git
 
   def install
-    bin.install 'bin/po'
-    share.install 'share/po-common'
-    share.install 'share/po-mac'
-    man1.install 'man/po.1'
+    bin.install "bin/po"
+    share.install "share/po-common"
+    share.install "share/po-mac"
+    man1.install "man/po.1"
     bash_completion.install "completion/po"
   end
 
   def caveats; <<~EOS
-  In order to complete the installation of po-util,
-  you must run 'po install'.
+    In order to complete the installation of po-util,
+    you must run "po install".
 
-  For Raspberry Pi support you must have docker installed.
-  EOS
+    For Raspberry Pi support you must have docker installed.
+    EOS
   end
 
   test do
