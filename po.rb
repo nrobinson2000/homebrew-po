2373fedb755131b4366412d84e9d94c26ffa2ad4
Nathan Robinson <nrobinson2000@me.com>
2018-04-23 23:33:31 -0400

    Update po.rb

--- po.rb
+++ po.rb
@@ -15,9 +15,9 @@
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
-  head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
   url 'https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz'
   sha256 'f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a'
+  head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
 
   def install
     bin.install 'bin/po'
@@ -28,11 +28,27 @@ class Po < Formula
   end
 
   def caveats; <<~EOS
-
   In order to complete the installation of po-util,
   you must run 'po install'.
 
   For Raspberry Pi support you must have docker installed.
   EOS
   end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test po`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+
+    system "po", "config", "release/v0.6.3", "duo", "false"
+    system "po", "info"
+    system "po", "install"
+  end
+
 end
