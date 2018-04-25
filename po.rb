7c3d85b56e472de6a2d39506813ee3f5665be760
Nathan Robinson <nrobinson2000@me.com>
2018-04-25 14:21:19 -0400

    Update po.rb

--- po.rb
+++ po.rb
@@ -1,7 +1,7 @@
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage "https://po-util.com"
-  url "https://github.com/nrobinson2000/po/archive/0.4.5.tar.gz"
+  url "https://github.com/nrobinson2000/po/archive/0.4.6.tar.gz"
   sha256 "fbe5e5825e33c0949e49007cd77d601b1674fa51de7b51677212c886b11a7dea"
   head "https://github.com/nrobinson2000/po", :using => :git
 
